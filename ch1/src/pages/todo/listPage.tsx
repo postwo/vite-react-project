9008c7fad6281dca0c7105673a0164ccf2f66666
postwo <codo7717@naver.com>
2025-08-22 21:28:34 +0900

    경로값 추출을 위한 useParams

--- ch1/src/pages/todo/listPage.tsx
+++ ch1/src/pages/todo/listPage.tsx
@@ -1,7 +1,22 @@
+import { useSearchParams } from 'react-router';
+
 function ListPage() {
+  const [queryParams] = useSearchParams();
+  const page: string | null = queryParams.get('page');
+  const size: string | null = queryParams.get('size');
+
+  //ex) http://localhost:5173/todo/list?page=2&size=10
   return (
     <div className="bg-white w-full">
-      <div className="text-4xl">Todo List Page</div>   {' '}
+      <div className="text-4xl">
+        Todo List Page {page} {size}
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="bg-white w-full">
+      <div className="text-4xl">Todo List Page</div>
     </div>
   );
 }
--- ch1/src/router/todoRouter.tsx
+++ ch1/src/router/todoRouter.tsx
@@ -1,8 +1,10 @@
 import { Component, lazy, Suspense } from 'react';
+import { Navigate } from 'react-router';
 
 const Loading = () => <div>Loading.....</div>;
 const TodoIndex = lazy(() => import('../pages/todo/indexPage'));
 const TodoList = lazy(() => import('../pages/todo/listPage'));
+const TodoRead = lazy(() => import('../pages/todo/readPage'));
 
 const todoRouter = () => {
   return {
@@ -16,6 +18,21 @@ const todoRouter = () => {
             <TodoList />
           </Suspense>
         ),
+      },
+      {
+        // 아이디에 해당하는 변수값을 tno라고 지정 변수명은 마음대로 변경 가능
+        //  중요한거는 tno 이렇게 받아오는것들은 다 문자열이다
+        path: 'read/:tno',
+        element: (
+          <Suspense fallback={<Loading />}>
+            <TodoRead />
+          </Suspense>
+        ),
+      },
+      {
+        // redirect 페이지 : redirect를 할때는 useNavigate를 사용 안하고 이렇게 처리한다
+        path: '',
+        element: <Navigate to={'/todo/list'}></Navigate>,
       },
     ],
   };
